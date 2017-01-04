871ce846d7d783c5337c3ae559482c7e073aba4e
Anders Jakobsen <aeh.jakobsen@gmail.com>
2017-01-04 12:04:38 +0100

    minor error

--- assignment1/assign1_tools/matmult_nat.c
+++ assignment1/assign1_tools/matmult_nat.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 
 void matmult_nat(int m,int n,int k,double **A,double **B,double **C){
-	for (int c = 0; c < reps; c++){
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int h = 0; h<k; h++){
-					C[i][j] += A[i][h]*B[h][j];
-				}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			C[i][j] = 0;
+			for (int h = 0; h<k; h++){
+				C[i][j] += A[i][h]*B[h][j];
 			}
 		}
 	}
