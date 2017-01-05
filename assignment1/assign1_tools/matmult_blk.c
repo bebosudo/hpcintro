c8eeabf4fc0e7ed90a3da265ff2ac908253fbe7b
miguelsuau <miguel.suau@gmail.com>
2017-01-05 09:39:15 +0000

    blk

--- assignment1/assign1_tools/matmult_blk.c
+++ assignment1/assign1_tools/matmult_blk.c
@@ -2,17 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define MIN(a, b)  (((a) < (b)) ? (a) : (b))
-
-
-void matmult_blk (int m,int n,int k,double **A,double **B,double **C){
+void matmult_blk (int m,int n,int k,double **A,double **B,double **C, int bs){
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
     		C[i][j] = 0;
     	}
-    }
-	int nbi = 5;
-	int nbj = 5;
-	int nbk = 5;
+  }
+	int nbi = bs;
+	int nbj = bs;
+	int nbk = bs;
 	for (int i1 = 0; i1 < m; i1+=nbi){
 		for (int j1 = 0; j1 < n; j1+=nbj){
 			for (int h1 = 0; h1 < k; h1+=nbk){
@@ -20,9 +18,9 @@ void matmult_blk (int m,int n,int k,double **A,double **B,double **C){
 					for (int j2 = 0; j2<MIN(n-j1,nbj); j2++){
 						for (int h2 = 0; h2<MIN(k-h1,nbk); h2++)
 							C[i1+i2][j1+j2] += A[i1+i2][h1+h2]*B[h1+h2][j1+j2];
-						}
 					}
 				}
 			}
 		}
 	}
+}
