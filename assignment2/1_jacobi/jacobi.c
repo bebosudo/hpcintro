94fc88ef41eb50744963697dd591828958fd4427
miguelsuau <miguel.suau@gmail.com>
2017-01-11 14:44:56 +0000

    modified main.c

--- assignment2/1_jacobi/jacobi.c
+++ assignment2/1_jacobi/jacobi.c
@@ -4,7 +4,6 @@ jacobi(double * unew, double * uold, double * f, double lambda, int N, int kmax,
   double d = treshold;
   int k;
   for (k = 0; (k < kmax || d > treshold); k++){
-    printf("--\n");
     for (int i = 1; i < N+1; i++){
       for (int j = 1; j < N+1; j++){
         unew[i*N+j] = ( 0.25*(uold[(i-1)*N+j]+uold[(i+1)*N+j]+
