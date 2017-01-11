12e6ac0a1a8d21fc2e35f1003d2d94d4ae2d0e3a
miguelsuau <miguel.suau@gmail.com>
2017-01-11 15:06:48 +0000

    modified main.c

--- assignment2/1_jacobi/main.c
+++ assignment2/1_jacobi/main.c
@@ -44,6 +44,12 @@ int main(int argc, char * argv[]){
   for (int i = 0; i < N+2; i++){
     printf("\n");
     for (int j = 0; j < N+2; j++){
+      printf("%.2lf ",f[i*(N+2)+j]);
+    }
+  }
+  for (int i = 0; i < N+2; i++){
+    printf("\n");
+    for (int j = 0; j < N+2; j++){
       printf("%.2lf ",unew[i*(N+2)+j]);
     }
   }
