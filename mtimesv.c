ba27c2571ac74752b81c7eec6668561b6a663352
Alberto Chiusole <s162501@student.dtu.dk>
2017-01-11 11:10:22 +0100

    2017-01-10 and 11 slides and exercises.

---
+++ mtimesv.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
+
+
+void mxv(int m, int n, double *a, double *b, double *c) {
+    int i, j;
+    double sum;
+
+//    #pragma omp parallel for default(none) \
+//                shared(m,n,a,b,c) private(i,j,sum)
+
+    for (i=0; i<m; i++) {
+        sum = 0.0;
+        for (j=0; j<n; j++) {
+            sum += b[i*n+j] * c[j];
+        }
+        a[i] = sum;
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+}
