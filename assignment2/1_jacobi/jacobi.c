29934aab73f877c6289469f4cacae232c9ceb123
miguelsuau <miguel.suau@gmail.com>
2017-01-11 14:26:43 +0000

    jacobi modified

--- assignment2/1_jacobi/jacobi.c
+++ assignment2/1_jacobi/jacobi.c
@@ -4,8 +4,8 @@ jacobi(double * unew, double * uold, double * f, double lambda, int N, int kmax,
   for (int k = 0; (k > kmax || d < treshold); k++){
     for (int i = 1; i < N+1; i++){
       for (int j = 1; j < N+1; j++){
-        unew[i*N+j] = ( 0.25*(unew[(i-1)*N+j]+uold[(i+1)*N+j]+
-                      unew[i*N+j-1]+uold[i*N+j+1]+lambda*lambda*f[i*N+j]) );
+        unew[i*N+j] = ( 0.25*(uold[(i-1)*N+j]+uold[(i+1)*N+j]+
+                      uold[i*N+j-1]+uold[i*N+j+1]+lambda*lambda*f[i*N+j]) );
         d = (unew[i*N+j]-uold[i*N+j])*(unew[i*N+j]-uold[i*N+j]);
       }
     }
--- assignment2/1_jacobi/main.c
+++ assignment2/1_jacobi/main.c
@@ -28,11 +28,11 @@ int main(int argc, char * argv[]){
     return -1;
   }
   for(int i = 0; i < N+1; i++){
-    uold[i*(N+1)] = 20;
-    uold[(N+1)+i*(N+1)] = 20;
+    uold[i*(N+2)] = 20;
+    uold[N+1+(N+2)*i] = 20;
     uold[i] = 20;
-    unew[i*(N+1)] = 20;
-    unew[(N+1)+i*(N+1)] = 20;
+    unew[i*(N+2)] = 20;
+    unew[(N+1)+i*(N+2)] = 20;
     unew[i] = 20;
   for (int i = N/2; i < 2*N/3+1; i++){
     for (int j = N/6; j < N/3+1; j++){
