c7d168f49c4aad6568bf52754b4ba772f2fe0412
miguelsuau <miguel.suau@gmail.com>
2017-01-12 17:19:47 +0000

    dsijfwe

--- assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
+++ assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
@@ -17,7 +17,7 @@ jacobi(double * unew, double * uold, double * f,
                 d = 0;
                 (*k)++;
             }
-            #pragma omp for private(i,j)
+            #pragma omp for
             for (int i = 1; i < N+1; i++) {
               for (int j = 1; j < N+1; j++) {
                 unew[i*M+j] = ( 0.25*(uold[(i-1)*M+j]+uold[(i+1)*M+j]+
@@ -25,7 +25,7 @@ jacobi(double * unew, double * uold, double * f,
               }
             } // implicit barrier here
 
-            #pragma omp for reduction(+: d) private(i,j)
+            #pragma omp for reduction(+: d)
             for (int i = 1; i < N+1; i++){
               for (int j = 1; j < N+1; j++){
                 d += (unew[i*M+j]-uold[i*M+j])*(unew[i*M+j]-uold[i*M+j]);
