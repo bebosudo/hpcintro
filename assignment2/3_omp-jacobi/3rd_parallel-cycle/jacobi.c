1e54f5cb93c36eaa45d1f983ed5ab8306f7d5f8b
Alberto Chiusole <s162501@student.dtu.dk>
2017-01-12 18:48:04 +0100

    Working version of jacobi correctly parallelized with OpenMP, and without explicit barriers.

--- assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
+++ assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
@@ -6,39 +6,49 @@ jacobi(double * unew, double * uold, double * f,
 
   double lambda2 = lambda*lambda;
   int M = N+2;
-  int i,j;
-  double d = treshold;
+  double d = treshold , diff = treshold;    // TODO add in the latex that we are
+        // using a different var `diff` to avoid using the same variable `d`
+        // both for checking whether to enter the loop and for storing the
+        // difference.. this is a problem because ....
   *k = 0;
 
-  printf("here\n");
-
-    #pragma omp parallel shared(d)
+    #pragma omp parallel shared(k, d, unew, uold, f, lambda2, M)
     {
-        while (*k < kmax && d >= treshold) {
-            printf("-");
-            #pragma omp master
+        while (*k < kmax && diff >= treshold) {
+
+//            #pragma omp barrier
+            #pragma omp single
             {
-                printf(".");
+//                printf("k=%d, d=%lf\n", *k, d);
+//                print
                 (*k)++;
                 d = 0;
+                diff = treshold+1;
             }
+//            #pragma omp barrier
 
-            #pragma omp for
-            for (i = 1; i < N+1; i++) {
-              for (j = 1; j < N+1; j++) {
+            #pragma omp for /*private(i, j)*/
+            for (int i = 1; i < N+1; i++) {
+              for (int j = 1; j < N+1; j++) {
                 unew[i*M+j] = ( 0.25*(uold[(i-1)*M+j]+uold[(i+1)*M+j]+
                               uold[i*M+j-1]+uold[i*M+j+1]+lambda2*f[i*M+j]) );
               }
             } // implicit barrier here
 
-            #pragma omp for reduction(+: d)
-            for (i = 1; i < N+1; i++){
-              for (j = 1; j < N+1; j++){
+            #pragma omp for /*private(i, j)*/ reduction(+: d)
+            for (int i = 1; i < N+1; i++){
+              for (int j = 1; j < N+1; j++){
                 d += (unew[i*M+j]-uold[i*M+j])*(unew[i*M+j]-uold[i*M+j]);
                 uold[i*M+j] = unew[i*M+j];
               }
             } // implicit barrier here
+
+            #pragma omp single nowait       // TODO check whether nowait is required.. but anyway it should improve the performances.
+            {
+                diff = d;
+            }
       }
   }
-
+        // TODO: ask whether the barriers exist only at the end of a directive or also at the beginning.
+//    printf("d:%lf\n", d);
 }
