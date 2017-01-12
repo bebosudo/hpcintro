228bd8bb2c935457d7f60bf1f1224c9be8f59d5d
Anders Jakobsen <aeh.jakobsen@gmail.com>
2017-01-12 11:54:37 +0100

    Changed to batch job

--- assignment2/1_jacobi/testN.sh
+++ assignment2/1_jacobi/testN.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-for N in 16 32 64 128 256 512 1024
+#PBS -N collector
+#PBS -q hpcintro
+#PBS -l walltime=0:05:00
+OUTFILE=timing.${PBS_JOBID}.txt
+#PBS -o $OUTFILE
+cd $PBS_O_WORKDIR
+module load studio
+
+
+for N in 16 32 64 128 256
 do
-	./1_jacobi $N 100000 0.001
+	./1_jacobi $N 100000 0.001 > $OUTFILE
 done
