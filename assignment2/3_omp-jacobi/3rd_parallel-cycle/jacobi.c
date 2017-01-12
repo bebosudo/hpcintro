fe3ec00df99c2d37a13ada5e113d65282d414932
miguelsuau <miguel.suau@gmail.com>
2017-01-12 17:18:08 +0000

    dsijfwe

--- assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
+++ assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
@@ -6,34 +6,28 @@ jacobi(double * unew, double * uold, double * f,
 
   double lambda2 = lambda*lambda;
   int M = N+2;
-  int i,j;
   double d = treshold;
-  *k = 0;
+  *k = kmax;
 
-  printf("here\n");
-
-    #pragma omp parallel shared(d)
+    #pragma omp parallel
     {
         while (*k < kmax && d >= treshold) {
-            printf("-");
-            #pragma omp master
+          #pragma omp single
             {
-                printf(".");
-                (*k)++;
                 d = 0;
+                (*k)++;
             }
-
-            #pragma omp for
-            for (i = 1; i < N+1; i++) {
-              for (j = 1; j < N+1; j++) {
+            #pragma omp for private(i,j)
+            for (int i = 1; i < N+1; i++) {
+              for (int j = 1; j < N+1; j++) {
                 unew[i*M+j] = ( 0.25*(uold[(i-1)*M+j]+uold[(i+1)*M+j]+
                               uold[i*M+j-1]+uold[i*M+j+1]+lambda2*f[i*M+j]) );
               }
             } // implicit barrier here
 
-            #pragma omp for reduction(+: d)
-            for (i = 1; i < N+1; i++){
-              for (j = 1; j < N+1; j++){
+            #pragma omp for reduction(+: d) private(i,j)
+            for (int i = 1; i < N+1; i++){
+              for (int j = 1; j < N+1; j++){
                 d += (unew[i*M+j]-uold[i*M+j])*(unew[i*M+j]-uold[i*M+j]);
                 uold[i*M+j] = unew[i*M+j];
               }
