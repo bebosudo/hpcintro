700d6e89ff1000944b744b96e77f2acc554c6bbc
Alberto Chiusole <s162501@student.dtu.dk>
2017-01-12 16:53:29 +0100

    2nd version of the omp jacobi.

---
+++ assignment2/3_omp-jacobi/2nd_parallel-cycle-orphaned/jacobi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void
+jacobi(double * unew, double * uold, double * f,
+      double lambda, int N, int kmax, double treshold, int * k){
+
+  double lambda2 = lambda*lambda;
+  int M = N+2;
+  int i,j;
+  double d = treshold+1;
+
+  for (*k = 0; (*k < kmax && d > treshold); (*k)++){
+      #pragma omp parallel
+      {
+        d = 0;
+        #pragma omp for
+        for (i = 1; i < N+1; i++) {
+          for (j = 1; j < N+1; j++) {
+            unew[i*M+j] = ( 0.25*(uold[(i-1)*M+j]+uold[(i+1)*M+j]+
+                          uold[i*M+j-1]+uold[i*M+j+1]+lambda2*f[i*M+j]) );
+          }
+        }
+
+        #pragma omp for reduction(+: d)
+        for (i = 1; i < N+1; i++){
+          for (j = 1; j < N+1; j++){
+            d += (unew[i*M+j]-uold[i*M+j])*(unew[i*M+j]-uold[i*M+j]);
+            uold[i*M+j] = unew[i*M+j];
+          }
+        }
+      }
+  }
+
+}
