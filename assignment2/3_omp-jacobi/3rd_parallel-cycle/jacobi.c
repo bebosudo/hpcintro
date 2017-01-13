583b8348881856b422c3c48755a60412b0eb2882
Alberto Chiusole <s162501@student.dtu.dk>
2017-01-13 11:57:19 +0100

    Better version, without copying matrices.

--- assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
+++ assignment2/3_omp-jacobi/3rd_parallel-cycle/jacobi.c
@@ -6,6 +6,7 @@ jacobi(double * unew, double * uold, double * f,
 
   double lambda2 = lambda*lambda;
   int M = N+2;
+  double* swapper;
   double diff = treshold;    // TODO add in the latex that we are
         // using a different var `diff` to avoid using the same variable `d`
         // both for checking whether to enter the loop and for storing the
@@ -16,30 +17,31 @@ jacobi(double * unew, double * uold, double * f,
     {
         while (*k < kmax && diff >= treshold) {
 
-            #pragma omp for
+            #pragma omp for reduction(+: diff)
             for (int i = 1; i < N+1; i++) {
               for (int j = 1; j < N+1; j++) {
                 unew[i*M+j] = ( 0.25*(uold[(i-1)*M+j]+uold[(i+1)*M+j]+
                               uold[i*M+j-1]+uold[i*M+j+1]+lambda2*f[i*M+j]) );
+                diff += (unew[i*M+j]-uold[i*M+j])*(unew[i*M+j]-uold[i*M+j]);
               }
             } // implicit barrier here
 
             #pragma omp single
             {
 //                printf("k=%d, diff=%lf\n", *k, diff);
-                (*k)++;
                 diff = 0;
+                (*k)++;
+                swapper = uold;
+                uold = unew;
+                unew = swapper;
             }
-
-            #pragma omp for reduction(+: diff)
-            for (int i = 1; i < N+1; i++){
-              for (int j = 1; j < N+1; j++){
-                diff += (unew[i*M+j]-uold[i*M+j])*(unew[i*M+j]-uold[i*M+j]);
-                uold[i*M+j] = unew[i*M+j];
-              }
-            } // implicit barrier here
       }
   }
+
+  swapper = unew;
+  unew = uold;
+  unew = swapper;
+
         // TODO: ask whether the barriers exist only at the end of a directive or also at the beginning.
 //    printf("d:%lf\n", d);
 }
