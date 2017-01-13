5a8ddd4dc5adacefc9394beecd2b36efd31a123a
Alberto Chiusole <s162501@student.dtu.dk>
2017-01-13 12:27:44 +0100

    Deleted old 1st implementation, no more usable. Updated submit jobs

--- assignment2/3_omp-jacobi/2nd_parallel-cycle/submit.sh
+++ assignment2/3_omp-jacobi/2nd_parallel-cycle/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 02614 - High-Performance Computing, January 2017
 # 
-# batch script to run collect on a decidated server in the hpcintro
+# batch script to run collect on a dedicated server in the hpcintro
 # queue
 #
 # Author: Bernd Dammann <bd@cc.dtu.dk>
@@ -17,28 +17,18 @@ cd $PBS_O_WORKDIR
 
 module load studio > /dev/null
 
-# experiment name 
-#
-#JID=`echo ${PBS_JOBID%.*}`
-#EXPOUT="$PBS_JOBNAME.${PERM}.${JID}.er"
-
-# uncomment the HWCOUNT line, if you want to use hardware counters
-# define an option string for the harwdware counters (see output of
-# 'collect -h' for valid values.  The format is:
-# -h cnt1,on,cnt2,on,...  (up to four counters at a time)
-#
-# the example below is for L1 hits, L1 misses, L2 hits, L2 misses
-#
-#HWCOUNT="-h dch,on,dcm,on,l2h,on,l2m,on"
-
 EXECUTABLE=3_omp_jacobi
-ITERATIONS_MAX=1000000
+ITERATIONS_MAX=100  # we use a small number of iterations in order to make the
+                    # program hit the stopping criterion, we don't need it to
+                    # reach the end of the whole calculation.
 THRESHOLD=0.001
 
-for N in 16 32 64 128 256
+
+for N in 128 256 512 1024 1260 2048 4096 8192 10000
 do
     for num_th in 1 2 4 8 16 32
     do
+        printf "${num_th} " >> $OUTFILE
         OMP_NUM_THREADS=${num_th} ./${EXECUTABLE} $N $ITERATIONS_MAX $THRESHOLD >> $OUTFILE
     done
 done
