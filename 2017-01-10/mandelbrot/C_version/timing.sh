1f88aac63c1fae1f8f2b6969ad6d549959a44655
Anders Jakobsen <aeh.jakobsen@gmail.com>
2017-01-13 10:14:25 +0100

    Changed to work as batch

--- 2017-01-10/mandelbrot/C_version/timing.sh
+++ 2017-01-10/mandelbrot/C_version/timing.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-for i in 1 2 4 8 16 24 32
+#PBS -N collector
+#PBS -q hpcintro
+#PBS -l walltime=0:10:00
+OUTFILE=timingMandelbrot.${PBS_JOBID}.txt
+#PBS -o $OUTFILE
+cd $PBS_O_WORKDIR
+module load studio
+
+for i in 1 2 4 8 16 32
 do
 	printf "$i "
-	OMP_NUM_THREADS=$i ./mandelbrot
+	OMP_NUM_THREADS=$i ./mandelbrot 1259 >> $OUTFILE
 done
