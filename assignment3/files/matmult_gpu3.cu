ac6566131adaccde1cb7cb2e01cb20467fdefd54
miguelsuau <miguel.suau@gmail.com>
2017-01-18 19:20:52 +0000

    sdw

--- assignment3/files/matmult_gpu3.cu
+++ assignment3/files/matmult_gpu3.cu
@@ -45,7 +45,7 @@ __global__ void m3_2(int m, int n, int k, double *A, double *B, double *C) {
         sum2 += A[i*k + h] * B[h*n + j+1];
       }
   C[i*n + j] = sum1;
-  C[i*n + j+1] = sum2;
+  C[i*n + j + 1] = sum2;
   }
 }
 
@@ -80,8 +80,8 @@ extern "C" {
         // Initialize the output matrix with zeroes.
         cudaMemset(d_C, 0, m*n * sizeof(double));
         dim3 BlockDim(16,16);
-        dim3 NumBlocks((m-1)/16+1,((n/2-1)/16+1));
-        m3_2<<<NumBlocks,BlockDim>>>(m, n, k, d_A, d_B, d_C);
+        dim3 NumBlocks((m/2-1)/16+1,((n-1)/16+1));
+        m3_1<<<NumBlocks,BlockDim>>>(m, n, k, d_A, d_B, d_C);
         cudaDeviceSynchronize();
 
         cudaMemcpy(C, d_C, m*n * sizeof(double), cudaMemcpyDeviceToHost);
