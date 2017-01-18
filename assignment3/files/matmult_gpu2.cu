faf361c763a4a27cc4b8010c175a7470dbe19f77
miguelsuau <miguel.suau@gmail.com>
2017-01-18 14:29:25 +0000

    efndklm

--- assignment3/files/matmult_gpu2.cu
+++ assignment3/files/matmult_gpu2.cu
@@ -19,11 +19,12 @@ __global__ void m2(int m, int n, int k, double *A, double *B, double *C) {
 
   int i = blockIdx.x*blockDim.x+threadIdx.x;
   int j = blockIdx.y*blockDim.y+threadIdx.y;
-
+  j *= 2;
   if (i < m && j < n){
-    for (int h = 0; h < k; h++) {
-      C[i*n + j] += A[i*k + h] * B[h*n + j];
-    }
+      for (int h = 0; h < k; h++) {
+        C[i*n + j] += A[i*k + h] * B[h*n + j];
+        C[i*n + j + 1] += A[i*k + h] * B[h*n + j + 1];
+      }
   }
 }
 
@@ -42,7 +43,7 @@ extern "C" {
         // Initialize the output matrix with zeroes.
         cudaMemset(d_C, 0, m*n * sizeof(double));
         dim3 BlockDim(16,16);
-        dim3 NumBlocks((m-1)/16+1,(n-1)/16+1);
+        dim3 NumBlocks((m-1)/16+1,((n-1)/16+1)/2);
         m2<<<NumBlocks,BlockDim>>>(m, n, k, d_A, d_B, d_C);
         cudaDeviceSynchronize();
 
