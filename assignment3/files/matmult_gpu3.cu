73e3f44b52357b9ffa61fa73666fee468c737676
miguelsuau <miguel.suau@gmail.com>
2017-01-19 13:43:30 +0000

    wensk

--- assignment3/files/matmult_gpu3.cu
+++ assignment3/files/matmult_gpu3.cu
@@ -46,7 +46,7 @@ __global__ void m3_2(int m, int n, int k, double *A, double *B, double *C) {
         sum2 += A[i*k + h] * B[h*n + j+1];
       }
   C[i*n + j] = sum1;
-  if (j+1 < n)
+  //if (j+1 < n)
    C[i*n + j + 1] = sum2;
   }
 }
@@ -82,7 +82,7 @@ extern "C" {
         // Initialize the output matrix with zeroes.
         cudaMemset(d_C, 0, m*n * sizeof(double));
         dim3 BlockDim(16,16);
-        dim3 NumBlocks((m/2-1)/16+1,((n-1)/16+1));
+        dim3 NumBlocks((m-1)/16+1,((n/2-1)/16+1));
 
         double time = omp_get_wtime();
         m3_1<<<NumBlocks,BlockDim>>>(m, n, k, d_A, d_B, d_C);
