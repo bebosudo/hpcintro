8bf3cfe1ce2791f051f5ca28fdb1616f0dcf51bb
miguelsuau <miguel.suau@gmail.com>
2017-01-19 17:22:42 +0000

    jfn

--- assignment3/files/matmult_gpu6.cu
+++ assignment3/files/matmult_gpu6.cu
@@ -50,7 +50,7 @@ extern "C" {
         cudaMemset(d_C, 0, m*n * sizeof(double));
         dim3 BlockDim(16,16);
         dim3 NumBlocks((m-1)/16+1,(n-1)/16+1);
-        m2<<<NumBlocks,BlockDim>>>(m, n, k, d_A, d_B, d_C);
+        m6<<<NumBlocks,BlockDim>>>(m, n, k, d_A, d_B, d_C);
         cudaDeviceSynchronize();
 
         cudaMemcpy(C, d_C, m*n * sizeof(double), cudaMemcpyDeviceToHost);
